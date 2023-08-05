2a9007c2633ccf8ef9c4e96636a2604fc41c5f5d
OniiCoder <oniicoder@gmail.com>
2023-08-05 15:42:51 +0000

    change input

--- resources/views/models/datefilter/form.blade.php
+++ resources/views/models/datefilter/form.blade.php
@@ -1,13 +1,16 @@
 <div>
     <div class="grid grid-cols-3 gap-4">
-        <x-fab::forms.datepicker
-            wire:model="startDate"
-            label="Start Date"
-        />
-        <x-fab::forms.datepicker
-            wire:model="endDate"
-            label="End Date"
-        />
+{{--        <x-fab::forms.datepicker--}}
+{{--            wire:model="startDate"--}}
+{{--            label="Start Date"--}}
+{{--        />--}}
+{{--        <x-fab::forms.datepicker--}}
+{{--            wire:model="endDate"--}}
+{{--            label="End Date"--}}
+{{--        />--}}
+        <input type="date" wire:model="startDate" class="rounded-lg border-2 border-gray-300" />
+        <input type="date" wire:model="endDate" class="rounded-lg border-2 border-gray-300" />
+
         <div class="flex flex-col justify-end">
             <div>
                 <x-fab::elements.button type="button" wire:click="applyFilter">Apply filter</x-fab::elements.button>
